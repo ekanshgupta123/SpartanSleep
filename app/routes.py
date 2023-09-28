8c403ebaf603af3f0ca18e997aca0c2c744593a0
Bianca Cervantes <cervbianca9810@gmail.com>
2023-09-27 21:56:43 -0700

    added login page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import spartan_app
 from flask import render_template
 
-@spartan_app.route('/')
+@spartan_app.route('/base',methods=['Get','Post'])
 def homePage():
     return render_template('index.html')
+
+@spartan_app.route('/login',methods=['Get','Post'])
+def login():
+    return render_template('login.html')
