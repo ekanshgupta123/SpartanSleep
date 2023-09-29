699e0ec8996b7c85869f6b895190d127524b7409
Cameron Harris <camharris831@gmail.com>
2023-09-28 20:45:21 -0700

    added sign up page and linked post request to mysql database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flaskext.mysql import MySQL
 
 spartan_app = Flask(__name__)
 spartan_app.static_url_path = '/static'
 spartan_app.static_folder = 'static'
+
+spartan_app.config['MYSQL_DATABASE_USER'] = 'root'
+spartan_app.config['MYSQL_DATABASE_PASSWORD'] = 'pass'
+spartan_app.config['MYSQL_DATABASE_DB'] = 'SpartanSleep'
+spartan_app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql = MySQL()
+mysql.init_app(spartan_app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from app import spartan_app
+from app import spartan_app, mysql
 from flask import render_template
+from flask import request, url_for, redirect
 
 @spartan_app.route('/',methods=['Get','Post'])
 def homePage():
@@ -8,3 +9,22 @@ def homePage():
 @spartan_app.route('/login',methods=['Get','Post'])
 def login():
     return render_template('login.html')
+
+
+@spartan_app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        first_name = request.form['first-name']
+        last_name = request.form['last-name']
+        email = request.form['email']
+        password = request.form['password']
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO Account (email, firstName, lastName, pass) VALUES (%s, %s, %s, %s)',
+                       (email, first_name, last_name, password))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('homePage'))
