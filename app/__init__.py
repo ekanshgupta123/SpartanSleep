89ef411889fe567417f94028d2c286aace948d38
ekanshgupta123 <ekanshgupta2003@gmail.com>
2023-09-29 13:51:08 -0700

    moving to sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,24 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from jinja2 import Environment
 
 spartan_app = Flask(__name__)
 spartan_app.static_url_path = '/static'
 spartan_app.static_folder = 'static'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+spartan_app.config.update(
+    SECRET_KEY='this-is-a-secret',
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),   #sets location of database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+)
+
+db = SQLAlchemy(spartan_app)
+migrate = Migrate(spartan_app,db)
+
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,48 @@
-from app import spartan_app
+from app import spartan_app, db
+from flask import render_template, redirect, flash, request, url_for, session
+from app.forms import SignupForm
+from app.models import User
 from flask import render_template
+from datetime import timedelta
 
-@spartan_app.route('/',methods=['Get','Post'])
+
+@spartan_app.before_request
+def before_request():
+    session.permanent = True
+    spartan_app.permanent_session_lifetime = timedelta(minutes=15) 
+
+@spartan_app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@spartan_app.route('/', methods=['GET','POST'])
 def homePage():
     return render_template('index.html')
 
-@spartan_app.route('/login',methods=['Get','Post'])
+@spartan_app.route('/login', methods=['GET','POST'])
 def login():
     return render_template('login.html')
+
+@spartan_app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    current_form = SignupForm()
+    if current_form.validate_on_submit():
+        user = User(
+            firstName=current_form.firstName.data,
+            lastName=current_form.lastName.data,
+            email=current_form.email.data,
+            password=current_form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account creation successful!')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=current_form)
+
+
+
+
+
+
+
