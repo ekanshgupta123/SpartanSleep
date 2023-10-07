83fd2242a7640af0222f443787c73925812cd951
Bianca Cervantes <cervbianca9810@gmail.com>
2023-10-06 21:03:03 -0700

    back-end for login (not complete)

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db
 from datetime import datetime
+from flask_login import login_manager
+from app import login
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +10,7 @@ class User(db.Model):
     email = db.Column(db.String(256), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
